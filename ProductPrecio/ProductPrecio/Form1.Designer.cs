6adecebfc9dd85416fe1d0fc4ee6073e7f14cf02
Sergio <sergioalgorry@hotmail.com>
2018-10-16 19:10:45 -0300

    Lista de Precios

---
+++ ProductPrecio/ProductPrecio/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ProductPrecio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(197, 40);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btAgregar.TabIndex = 0;
+            this.btAgregar.Text = "&Agregar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.btAgregar_Click);
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(13, 13);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(175, 20);
+            this.txtProducto.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(214, 12);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(58, 20);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.btAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProductPrecio/ProductPrecio/Producto.cs
@@ -0,0 +1,121 @@
+﻿namespace ProductPrecio
+{
+    public class Producto
+    {
+        System.String[] Nombre = new System.String[1];
+        System.Decimal[] Precio = new System.Decimal[1];
+
+        System.Int32 CantElem = 0;
+
+
+        /// <summary>
+        /// carga un elemento nuevo a la lista
+        /// </summary>
+        /// <param name="aElemento">Elemento a cargar en la lista</param>
+        /// <returns></returns>
+        public System.String CargarLista(System.String aNombre,
+                                         System.String aPrecio)
+        {
+            System.String mRes = "";
+            System.Decimal mPrecio = System.Convert.ToDecimal(aPrecio);
+            
+            if (CantElem == Nombre.Length)
+            {
+                Nombre = RedimensionarStr(Nombre);
+                Precio = RedimensionarDec(Precio);
+            }
+            if (BuscaPorNombre(aNombre) == -1)
+            {
+                Nombre[CantElem] = aNombre;
+                Precio[CantElem] = mPrecio;
+                CantElem++;
+                mRes = Listado();
+            }
+            else
+            {
+                mRes = "El elemento " + aNombre + " ya existe";
+            }
+            return mRes;
+        }
+
+        /// <summary>
+        /// devuelve en un string los elemntos de laa lista
+        /// </summary>
+        /// <returns></returns>
+        public System.String Listado()
+        {
+            System.String Res = "Productos:\r\n";
+            System.Int32 j = 0;
+            foreach (System.String mElemento in Nombre)
+            {
+                if (mElemento == null)
+                {
+                    break;
+                }
+                Res = Res + Precio[j].ToString() 
+                    + " - " 
+                    + mElemento + "\r\n";
+                j = j + 1;
+            }
+
+            return Res;
+
+        }
+
+        /// <summary>
+        /// devuelve la posicion donde se encuentra un elemento dentro de la lista, 
+        /// si el valor devuelto es -1 significa que no se encontro
+        /// </summary>
+        /// <param name="aElemento">elemnto a buscar en la lista</param>
+        /// <returns></returns>
+        public System.Int32 BuscaPorNombre(System.String aElemento)
+        {
+            System.Int32 mREs = -1;
+
+            for (int i = 0; i < Nombre.Length; i++)
+            {
+                if (Nombre[i] == aElemento)
+                {
+                    mREs = i + 1;
+                    break;
+                }
+            }
+            return mREs;
+        }
+
+        /// <summary>
+        /// Agrega un elemento null a la lista al final
+        /// </summary>
+        /// <param name="aLista">Lista de string a redimensionar</param>
+        private System.String[] RedimensionarStr(System.String[] aLista)
+        {
+            System.String[] Res = new System.String[aLista.Length + 1];
+            System.Int32 j = 0;
+            foreach (System.String mElemento in aLista)
+            {
+                Res[j] = mElemento;
+                j++;
+            }
+
+            return Res;
+        }
+
+        /// <summary>
+        /// Agrega un elemento null a la lista al final
+        /// </summary>
+        /// <param name="aLista">Lista de descimal a redimensionar</param>
+        /// <returns></returns>
+        private System.Decimal[] RedimensionarDec(System.Decimal[] aLista)
+        {
+            System.Decimal[] Res = new System.Decimal[aLista.Length + 1];
+            System.Int32 j = 0;
+            foreach (System.Decimal mElemento in aLista)
+            {
+                Res[j] = mElemento;
+                j++;
+            }
+
+            return Res;
+        }
+    }
+}
